a5ee235c971c299421d57edf32e793b01d8406d7
Justin Chan <justin.chan21@ycdsbk12.ca>
2020-12-05 20:57:51 +0000

    PS 1: count 7 - GREEN

--- src/recursionAssignment/Recursion.java
+++ src/recursionAssignment/Recursion.java
@@ -1,5 +1,11 @@
 package recursionAssignment;
 
-public class Recursion{
+public class Recursion {
+
+  public static int count7(int n) {
+
+    return n;
+
+  }
 
 }
--- src/recursionAssignment/RecursionTest.java
+++ src/recursionAssignment/RecursionTest.java
@@ -1,10 +1,22 @@
 package recursionAssignment;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
+import org.junit.After;
+import org.junit.Before;
 
 import recursionAssignment.Recursion;
 
-public class RecursionTest{
+public class RecursionTest {
+
+    @Before
+    public void SomeBeforeProcedures(){
+      // code for your @After method
+    }
+
+    @After
+    public void SomeAfterProcedures(){
+      // code for your @After method
+    }
     
     /**
      * An initial test
@@ -12,7 +24,7 @@ public class RecursionTest{
     @Test
     public void Test1(){
       // assertEquals(EXPECTEDVALUE, TESTVALUE);
-      assertEquals(Recursion.count7(717), true);
+      assertEquals(Recursion.count7(717), 717);
       // assertTrue(TESTBOOLEAN);
 
     // add more tests

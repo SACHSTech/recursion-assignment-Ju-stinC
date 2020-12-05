d2d866881308bca7e13d4fe0fa5b8f1be7911b68
Justin Chan <justin.chan21@ycdsbk12.ca>
2020-12-05 01:45:59 +0000

    PS 1: count 7 - RED

--- src/recursionAssignment/RecursionTest.java
+++ src/recursionAssignment/RecursionTest.java
@@ -1,34 +1,21 @@
 package recursionAssignment;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.Before;
-import org.junit.jupiter.api.After;
 
 import recursionAssignment.Recursion;
 
 public class RecursionTest{
-    
-    @Before
-    public void beforeTest(){
-      // some steps to perform before tests are run
-    }
     
     /**
      * An initial test
      */
     @Test
     public void Test1(){
-        // make assertion statement(s)
-        // assertEquals(EXPECTEDVALUE, TESTVALUE);
-        // assertTrue(TESTBOOLEAN);
-    }
-    
+      // assertEquals(EXPECTEDVALUE, TESTVALUE);
+      assertEquals(Recursion.count7(717), true);
+      // assertTrue(TESTBOOLEAN);
+
     // add more tests
-    
-    @After
-    public void afterTest(){
-      // some steps to perform after tests are run
+
     }
-    
-    
 }

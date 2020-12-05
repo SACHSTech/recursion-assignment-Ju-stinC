0b3da38f419b68b2a1cbe0a0d8597a0503381a4f
Justin Chan <justin.chan21@ycdsbk12.ca>
2020-12-05 21:36:43 +0000

    PS 1: count 7 - GREEN2

--- src/recursionAssignment/Recursion.java
+++ src/recursionAssignment/Recursion.java
@@ -4,10 +4,14 @@ public class Recursion {
 
   public static int count7(int n) {
 
-    if(n == 7) {
+    if (n == 7) {
 
       return 1;
       
+    }else if (n != 7) {
+
+      return 0;
+
     }
 
     return n;

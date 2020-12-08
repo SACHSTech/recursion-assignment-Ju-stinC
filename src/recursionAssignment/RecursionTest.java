c182369ba29364e99784b1b071fab5dada21d01c
Justin Chan <justin.chan21@ycdsbk12.ca>
2020-12-08 00:47:42 +0000

    PS 3: array220 - RED

--- src/recursionAssignment/RecursionTest.java
+++ src/recursionAssignment/RecursionTest.java
@@ -29,53 +29,58 @@ public class RecursionTest {
 
     @Test
     public void Test2(){
-    assertEquals(Recursion.count7(7), 1);
+      assertEquals(Recursion.count7(7), 1);
     }
 
     @Test
     public void Test3(){
-    assertEquals(Recursion.count7(123), 0);
+      assertEquals(Recursion.count7(123), 0);
     }
 
     @Test
     public void Test4(){
-    assertEquals(Recursion.count7(75854773), 3);
+      assertEquals(Recursion.count7(75854773), 3);
     }
 
     @Test
     public void Test5(){
-    assertEquals(Recursion.count7(-1), 0);
+      assertEquals(Recursion.count7(-1), 0);
     }
 
     @Test
     public void Test6(){
-    assertEquals(Recursion.count7(0), 0);
+      assertEquals(Recursion.count7(0), 0);
     }
 
     @Test
     public void Test7(){
-    assertEquals("", Recursion.changePi(""));
+      assertEquals("", Recursion.changePi(""));
     // when str is an empty string (initial red state)
     }
 
     @Test
     public void Test8(){
-    assertEquals("x3.14x", Recursion.changePi("xpix"));
+      assertEquals("x3.14x", Recursion.changePi("xpix"));
     }
 
     @Test
     public void Test9(){
-    assertEquals("3.143.14", Recursion.changePi("pipi"));
+      assertEquals("3.143.14", Recursion.changePi("pipi"));
     }
 
     @Test
     public void Test10(){
-    assertEquals("3.14p", Recursion.changePi("pip"));
+      assertEquals("3.14p", Recursion.changePi("pip"));
     }
 
     @Test
     public void Test11(){
     assertEquals("p", Recursion.changePi("p"));
     // when the length of str is less than 2
+    }
+
+    @Test
+    public void Test12(){
+    assertEquals(true, Recursion.array220(new int[] {1, 2, 20}, 0));
     }
 }
